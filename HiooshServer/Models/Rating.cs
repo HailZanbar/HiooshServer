9839f4453bda5608b56c263ca1bc3fabc20cb659
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-05-08 23:02:02 +0300

    i made the base of the rating part

---
+++ HiooshServer/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿namespace HiooshServer.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+
+        public int NumberRating { get; set; }
+
+        public string StringRating { get; set; }
+
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+    }
+}
