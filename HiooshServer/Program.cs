45fffff52666c2bfd65a7202b293b4db13cfd706
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-05-10 16:19:02 +0300

    i finsh the rating part! yaii

--- HiooshServer/Program.cs
+++ HiooshServer/Program.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using HiooshServer.Data;
+using HiooshServer.Services;
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddDbContext<HiooshServerContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("HiooshServerContext") ?? throw new InvalidOperationException("Connection string 'HiooshServerContext' not found.")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
