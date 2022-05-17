7c4da9c79f92be0d376bb1b456e619452ece57f0
hail zanbar <hailzanbar1@gmail.com>
2022-05-17 13:04:57 +0300

    hi

--- HiooshServer/Models/Message.cs
+++ HiooshServer/Models/Message.cs
@@ -2,22 +2,16 @@
 {
     public class Message
     {
-        public Message(int _id, string _type, string _content, string _from, string _to, string _time, string _date)
+        public Message(int _id, string _content, bool _sent, string _created)
         {
             id = _id;
-            type = _type;
             content = _content;
-            from = _from;
-            to = _to;
-            time = _time;
-            date = _date;
+            sent = _sent;
+            created = _created;
         }
         public int id { get; set; }
-        public string type { get; set; }
         public string content { get; set; }
-        public string from { get; set; }
-        public string to { get; set; }
-        public string time { get; set; }
-        public string date { get; set; }
+        public bool sent { get; set; }
+        public string created { get; set; }
     }
 }
