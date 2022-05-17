3040304962ad976b4c777ad26ce48545da514e7a
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-05-17 13:18:23 +0300

    the transferControll is perfect

--- HiooshServer/Controllers/TransferController.cs
+++ HiooshServer/Controllers/TransferController.cs
@@ -17,8 +17,10 @@ namespace HiooshServer.Controllers
         public IActionResult AddMessage(string from, string to, string content)
         {
             if (!ModelState.IsValid)
-            {
-                Message message = new Message(int _id, content, false, DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
+            { 
+                List<Message> messages = _contactsService.GetMessages(to, from);
+                int id_of_last = messages[messages.Count-1].id;
+                Message message = new Message(id_of_last++, content, false, DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
                 _contactsService.AddMessage(to, from, message);
                 return Created(string.Format("/api/tranfer/{0}", message.id), message);
             }
