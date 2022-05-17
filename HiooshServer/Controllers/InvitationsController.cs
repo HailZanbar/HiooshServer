d624a6f7581c973b4b8b942cb37a6c4ce6aa4769
Hadas Eshel Sofer <hadseshel@gmail.com>
2022-05-17 13:06:37 +0300

    heyy

---
+++ HiooshServer/Controllers/InvitationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HiooshServer.Services;
+using HiooshServer.Models;
+
+namespace HiooshServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InvitationsController : Controller
+    {
+        private readonly IContactsService _contactsService;
+
+        public InvitationsController(IContactsService contactsService)
+        {
+            _contactsService = contactsService;
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(string from, string to, string server)
+        {
+            if (!ModelState.IsValid)
+            {
+                Contact contact = new Contact(from,from,server);
+                 _contactsService.AddContact(from, contact);
+                 return Created(string.Format("/api/invitations/{0}", contact.id), contact);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ HiooshServer/Controllers/TransferController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HiooshServer.Services;
+using HiooshServer.Models;
+
+namespace HiooshServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransferController : Controller
+    {
+        private readonly IContactsService _contactsService;
+        public TransferController(IContactsService contactsService)
+        {
+            _contactsService = contactsService;
+        }
+        [HttpPost]
+        public IActionResult AddMessage(string from, string to, string content)
+        {
+            if (!ModelState.IsValid)
+            {
+                Message message = new Message(int _id, content, false, DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
+                _contactsService.AddMessage(to, from, message);
+                return Created(string.Format("/api/tranfer/{0}", message.id), message);
+            }
+            return BadRequest();
+        }
+    }
+}
